e6ae3b7d02554c27851b8378256189a42b9b2553
Visanth1135 <k.visanth1996@gmail.com>
2022-07-26 11:07:39 +0400

    UI edit

--- project_e_commerce-main/src/components/Products/Product/Product.jsx
+++ project_e_commerce-main/src/components/Products/Product/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
+import './productStyles.css'
 
 
 import useStyles from './styles';
@@ -11,25 +12,67 @@ const Product = ({ product, onAddToCart }) => {
   const handleAddToCart = () => onAddToCart(product.id, 1);
 
   return (
-    <Card className={classes.root}>
-    <CardMedia className={classes.media} image={product.media.source} title={product.name} />
-    <CardContent>
-      <div className={classes.cardContent}>
-        <Typography gutterBottom variant="h5" component="h2">
-          {product.name}
-        </Typography>
-        <Typography gutterBottom variant="h5" component="h2">
-          ${product.price.formatted}
-        </Typography>
+    <div class="wrapper">
+  <div class="container">
+    <div class="top"><img src = {product.media.source}></img></div>
+    <div class="bottom">
+      <div class="left">
+        <div class="details">
+          <h1>Chair</h1>
+          <p>£250</p>
+        </div>
+        <div class="buy"><i class="material-icons"><AddShoppingCart/></i></div>
       </div>
-      <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
-    </CardContent>
-    <CardActions disableSpacing className={classes.cardActions}>
-      <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
-        <AddShoppingCart />
-      </IconButton>
-    </CardActions>
-  </Card>
+      <div class="right">
+        <div class="done"><i class="material-icons">done</i></div>
+        <div class="details">
+          <h1>Chair</h1>
+          <p>Added to your cart</p>
+        </div>
+        <div class="remove"><i class="material-icons">clear</i></div>
+      </div>
+    </div>
+  </div>
+  <div class="inside">
+    <div class="icon"><i class="material-icons">info_outline</i></div>
+    <div class="contents">
+      <table>
+        <tr>
+          <th>Width</th>
+          <th>Height</th>
+        </tr>
+        <tr>
+          <td>3000mm</td>
+          <td>4000mm</td>
+        </tr>
+        <tr>
+          <th>Something</th>
+          <th>Something</th>
+        </tr>
+        <tr>
+          <td>200mm</td>
+          <td>200mm</td>
+        </tr>
+        <tr>
+          <th>Something</th>
+          <th>Something</th>
+        </tr>
+        <tr>
+          <td>200mm</td>
+          <td>200mm</td>
+        </tr>
+        <tr>
+          <th>Something</th>
+          <th>Something</th>
+        </tr>
+        <tr>
+          <td>200mm</td>
+          <td>200mm</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
   );
 };
 
